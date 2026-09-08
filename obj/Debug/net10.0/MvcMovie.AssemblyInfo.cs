e6faeadb50b0179f72c70bd101ade5a2157e6646
Fluffy-katy <154894474+Fluffy-katy@users.noreply.github.com>
2026-09-08 14:36:08 +0300

    Part 5, work with a database

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c62a9fed500e52c175eb1c4bdb16fdb59cf9039b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ae4c3e75bd84709fe73eabd638642f52814e9fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
